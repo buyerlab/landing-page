b678621b4a5c59bb0650faabd727f87a532b64d2
henry-bao <henrybao@uw.edu>
2023-05-05 16:57:48 -0700

    fix spacing on hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -35,7 +35,7 @@ export default function Hero() {
                             className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
                             data-aos="zoom-y-out"
                         >
-                            Make your shopping experience
+                            Make your shopping experience{' '}
                             <span className="bg-clip-text text-transparent bg-gradient-to-r from-amber-500 to-amber-300">
                                 wonderful
                             </span>

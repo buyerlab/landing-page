54aee51102d449135e826eaac5f518c986bd0982
henry-bao <henrybao@uw.edu>
2023-05-05 16:59:41 -0700

    grid spacing

--- components/stakeholders.tsx
+++ components/stakeholders.tsx
@@ -19,7 +19,7 @@ export default function Stakeholders() {
                     </div>
 
                     {/* Items */}
-                    <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
+                    <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-1 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
                         {/* 1st item */}
                         <div
                             className="relative flex flex-col items-center p-6 bg-gradient-to-r from-gray-100 to-amber-100 rounded shadow-xl"
